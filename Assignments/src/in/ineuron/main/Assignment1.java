531d91467eec1288e49e7b62599438b44f907d6a
Viswanath <Viswanath@LAPTOP-CN4V5VA9>
2022-11-09 18:56:06 +0530

    First Commit

---
+++ Assignments/src/in/ineuron/main/Assignment1.java
@@ -0,0 +1,10 @@
+package in.ineuron.main;
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World...!!!");
+	}
+
+}
