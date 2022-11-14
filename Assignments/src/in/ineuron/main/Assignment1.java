03b598bc25225c4b533b8605de9381b5dbf38dde
Viswanath <Viswanath@LAPTOP-CN4V5VA9>
2022-11-14 18:01:44 +0530

    ass1

--- Assignments/src/in/ineuron/main/Assignment1.java
+++ Assignments/src/in/ineuron/main/Assignment1.java
@@ -4,8 +4,88 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World...!!!");
-		System.out.println("First Program");
+		int n=13;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(i==0 || i== n-1 || j==(n-1)/2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}		
+
+				for(int j=0;j<2*n;j++) {
+					if(i==j){
+					System.out.print("       ");
+					}
+				}
+
+			for(int j=0; j<n; j++)  
+			{
+				if(j==0 || j==n-1 || i==j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++)
+			{
+				if(j==0 || (i==0 && j<= (n-1)/2) || (i==(n-1)/2 && j<= (n-1)/2) || i==n-1 && j<= (n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++)
+			{
+				if((j==0 && i<n-1) || (i==n-1 && j>0 && j<n-1) || (j== n-1 && i<n-1))
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++)
+			{
+				if(j==0 || (i==0 && j<= (n-1)/2) || (j==(n-1)/2 && i<=(n-1)/2) || (i==(n-1)/2 && j<= (n-1)/2) || (i-j)==(n-1)/2)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++)
+			{
+				if(i==0 || i== n-1 || j==0 || j== n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			for(int j=0; j<n; j++)
+			{
+				if(j==0 || j==n-1 || i==j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+		System.out.println();	
+		}
 	}
 
 }
